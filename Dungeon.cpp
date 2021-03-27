ca0565ee1abf182c0337b25f3285845c65234457
Antonio Alcaide <aaflabs@gmail.com>
2021-03-27 10:49:58 +0100

    add loot actions

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -15,6 +15,39 @@ void Dungeon::printActions(int numActions, string actions[]) {
     
 }
 
+void Dungeon::handleLootActions(Room * room) {
+    Item item = room->items.front();
+    int  size = room->items.size();
+    player.lootRoom(room);
+    room->clearLoot();
+    for (int i = 0; i < size; i++) {
+        cout << "Has abierto un cofre y has encontrado un " << item.name << ".\n";     
+        cout << "Tu salud es ahora" << player.currentHealth << ", tu ataque es ahora " << player.attack << " y tu defensa " << player.defence << ".\n";
+    }
+    
+}
+
+void Dungeon::handleRoomWithChest(Room * room) {
+    cout << "Has entrado en una habitación con un gran tesoro...\n";
+    string actions [] = {
+        "a. Recoger el tesoro.",
+        "b. Moverse a otra habitacion."
+        };
+    while (true) {
+        printActions(2, actions);
+        string input;
+        cin >> input;
+        if (input == "a") {
+            handleLootActions(room);
+            return;
+        } else if (input == "b") {
+            return;
+        } else {
+            cout << "Accion incorrecta \n";
+        }   
+    }
+}
+
 void Dungeon::handleEmptyRoom(Room * room) {
     cout << "Has entrado en una habitación pero esta vacia...\n";
     string actions [] = {"a. Moverse a otra habitacion."};
@@ -34,9 +67,9 @@ void Dungeon::enterRoom(Room * room) {
     if (room->enemies.size() != 0) {
         // handle room with enemy
     } else if (room->items.size() != 0) {
-        // handle room with chest
+        handleRoomWithChest(room);
     } else {
-        // handle empty room
+        handleEmptyRoom(room);
     }
 }
 
