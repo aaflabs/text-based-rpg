e982337da9abb3a2f21567205ea1e69b0a2dbe74
Antonio Alcaide <aaflabs@gmail.com>
2021-03-27 11:15:02 +0100

    implement the attacks

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -15,6 +15,65 @@ void Dungeon::printActions(int numActions, string actions[]) {
     
 }
 
+void Dungeon::handleFightActions(GameCharacter * enemy) {
+    string actions [] = {
+        "a. Atacar",
+        "b. Retirarse"
+        };
+    while (true) {
+        printActions(2, actions);
+        string input;
+        cin >> input;
+        if (input == "a") {
+            int damage = enemy->takeDamage(player.attack);
+            cout << "Tu ataque hace " << damage << " de daño. \n";
+        } else if (input == "b") {
+            player.changeRooms(player.previousRoom);
+            enterRoom(player.currentRoom);
+            return;
+        } else {
+            cout << "Accion incorrecta \n";
+        }
+
+        if (enemy->checkIsDead())  {
+            cout << "Has ganado, tu enemigo " << enemy->name << " ha sido derrotado  \n";
+            player.increaseStats(10, 5, 5);
+            player.currentRoom->clearEnemies();
+            return;
+        }
+
+        int damage = player.takeDamage(enemy->attack);
+        cout << "El ataque de tu enemigo " << enemy->name << " hace " << damage << " de daño. \n";
+        cout << "Ahora tu salud es " << player.currentHealth << ". \n";
+        if (player.checkIsDead()) {
+            return;
+        }
+    }
+}
+
+void Dungeon::handleRoomWithEnemy(Room * room) {
+    GameCharacter enemy = room->enemies.front();
+    cout << "Has entrado en una habitación y ves un..." << enemy.name << "\n";
+    string actions [] = {
+        "a. Pelear contra " + enemy.name,
+        "b. Moverse a la habitacion anterior."
+        };
+    while (true) {
+        printActions(2, actions);
+        string input;
+        cin >> input;
+        if (input == "a") {
+            handleFightActions(&enemy);
+        } else if (input == "b") {
+            player.changeRooms(player.previousRoom);
+            enterRoom(player.currentRoom);
+            return;
+        } else {
+            cout << "Accion incorrecta \n";
+        }   
+    }
+}
+
 void Dungeon::handleLootActions(Room * room) {
     Item item = room->items.front();
     int  size = room->items.size();
@@ -65,7 +124,7 @@ void Dungeon::handleEmptyRoom(Room * room) {
 
 void Dungeon::enterRoom(Room * room) {
     if (room->enemies.size() != 0) {
-        // handle room with enemy
+        handleRoomWithEnemy(room);
     } else if (room->items.size() != 0) {
         handleRoomWithChest(room);
     } else {
